644519f8f8383f443227bee70b65931eed3b9da4
Adrian Alvarez <adriangalvarez@gmail.com>
2018-03-27 10:35:52 -0300

    Agregar archivos necesarios para implementar Toolbar y Tabs.

---
+++ app/src/main/java/com/adriangalvarez/listadecompras/Fragments/ComprasFragment.java
@@ -0,0 +1,23 @@
+package com.adriangalvarez.listadecompras.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.adriangalvarez.listadecompras.R;
+
+public class ComprasFragment extends Fragment{
+
+	public ComprasFragment(){
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ){
+		View view = inflater.inflate( R.layout.fragment_compras, container, false );
+		return view;
+	}
+
+}
